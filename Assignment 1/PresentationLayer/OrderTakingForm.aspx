34ed0959425928b27a372bc672bbbf7f4ca9f167
Saqib Farooq <saqib.farooq@hotmail.com>
2020-08-18 21:52:30 +0500

    Add project files.

---
+++ Assignment 1/PresentationLayer/OrderTakingForm.aspx
@@ -0,0 +1,168 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="OrderTakingForm.aspx.cs" Inherits="Assignment_1.PresentationLayer.OrderTakingForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <asp:Button ID="btnFirst" runat="server" Text="First" OnClick="btnFirst_Click" />
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btnPrev" runat="server" Text="Prev" OnClick="btnPrev_Click" />
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btnNext" runat="server" Text="Next" OnClick="btnNext_Click" />
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btnLast" runat="server" Text="Last" OnClick="btnLast_Click" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label runat="server" Text="Customer ID"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tbCustomerID" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label runat="server" Text="Name"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tbName" runat="server" ReadOnly="False"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label runat="server" Text="Address"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="tbAddress" runat="server" TextMode="MultiLine" ReadOnly="False"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label runat="server" Text="CompanyName"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tbCompanyName" runat="server" ReadOnly="False"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label runat="server" Text="Contact Title"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tbContactTitle" runat="server" ReadOnly="False"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label runat="server" Text="City"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tbCity" runat="server" ReadOnly="False"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label runat="server" Text="Region"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tbRegion" runat="server" ReadOnly="False"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label runat="server" Text="Postal Code"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tbPostalCode" runat="server" ReadOnly="False"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label runat="server" Text="Country"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tbCountry" runat="server" ReadOnly="False"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label runat="server" Text="Phone"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tbPhone" runat="server" ReadOnly="False"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label runat="server" Text="Fax"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tbFax" runat="server" ReadOnly="False"></asp:TextBox>
+                    </td>
+                </tr>
+            </table>
+            <br/>
+            <asp:GridView ID="gvOrders" runat="server">
+                <Columns>
+                    <asp:BoundField DataField="ProductName" HeaderText="Orders"/>
+                </Columns>
+            </asp:GridView>
+        </div>
+        <br/>
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <asp:Button ID="btnNew" runat="server" Text="New" OnClick="btnNew_Click" />
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btnEdit" runat="server" Text="Edit" OnClick="btnEdit_Click" />
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <br/>
+            <br/>
+            <asp:Label runat="server" Text="Select Products"></asp:Label>
+            <br/>
+            <asp:DropDownList ID="ddlProducts" runat="server"></asp:DropDownList>
+            <asp:Button ID="btnPlaceOrder" runat="server" Text="Place Order" OnClick="btnPlaceOrder_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Assignment 1/PresentationLayer/OrderTakingForm.aspx.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_1.PresentationLayer
+{
+    public partial class OrderTakingForm : System.Web.UI.Page
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=20.200.20.10;Initial Catalog=Northwind-QA;User ID=diyatech;Password=4Islamabad");
+        protected Boolean isUpdate = false;
+
+        public string CustomerId => tbCustomerID.Text.Trim();
+        
+        public void CreateEntry()
+        {
+            if (sqlConnection.State == ConnectionState.Closed)
+                sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("CustomerCreate", sqlConnection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@CustomerID", tbCustomerID.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@CompanyName", tbCompanyName.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@ContactName", tbName.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@Address", tbAddress.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@ContactTitle", tbContactTitle.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@City", tbCity.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@Region", tbRegion.Text.Trim());
+            Console.WriteLine("All Okay");
+            sqlCommand.Parameters.AddWithValue("@PostalCode", (tbPostalCode.Text.Trim()));
+            Console.WriteLine(tbPostalCode);
+            sqlCommand.Parameters.AddWithValue("@Country", tbCountry.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@Phone", tbPhone.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@Fax", ((tbFax.Text.Trim())));
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+        }
+
+        public void UpdateEntry()
+        {
+            if (sqlConnection.State == ConnectionState.Closed)
+                sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("CustomerUpdate", sqlConnection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@CustomerID", tbCustomerID.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@CompanyName", tbCompanyName.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@ContactName", tbName.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@Address", tbAddress.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@ContactTitle", tbContactTitle.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@City", tbCity.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@Region", tbRegion.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@PostalCode", (tbPostalCode.Text.Trim()));
+            sqlCommand.Parameters.AddWithValue("@Country", tbCountry.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@Phone", tbPhone.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@Fax", (tbFax.Text.Trim()));
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+        }
+
+        public void Clear()
+        {
+            tbCustomerID.Text = tbCompanyName.Text = tbName.Text = tbContactTitle.Text = "";
+            tbAddress.Text = tbCity.Text = tbRegion.Text = tbPostalCode.Text = "";
+            tbCountry.Text = tbPhone.Text = tbFax.Text = "";
+
+            btnEdit.Enabled = false;
+            btnDelete.Enabled = false;
+
+            tbCustomerID.ReadOnly = false;
+            tbCompanyName.ReadOnly = false;
+            tbName.ReadOnly = false;
+            tbContactTitle.ReadOnly = false;
+            tbAddress.ReadOnly = false;
+            tbCity.ReadOnly = false;
+            tbRegion.ReadOnly = false;
+            tbPostalCode.ReadOnly = false;
+            tbCountry.ReadOnly = false;
+            tbPhone.ReadOnly = false;
+            tbFax.ReadOnly = false;
+        }
+
+        public void FillGridView()
+        {
+
+            if (sqlConnection.State == ConnectionState.Closed)
+                sqlConnection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("CustomerOrderDetails", sqlConnection);
+            sqlDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@CustomerID", tbCustomerID.Text.Trim().ToCharArray());
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.Close();
+            gvOrders.DataSource = dataTable;
+            gvOrders.DataBind();
+
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnSave.Enabled = false;
+
+            tbCustomerID.ReadOnly = true;
+            tbCompanyName.ReadOnly = true;
+            tbName.ReadOnly = true;
+            tbContactTitle.ReadOnly = true;
+            tbAddress.ReadOnly = true;
+            tbCity.ReadOnly = true;
+            tbRegion.ReadOnly = true;
+            tbPostalCode.ReadOnly = true;
+            tbCountry.ReadOnly = true;
+            tbPhone.ReadOnly = true;
+            tbFax.ReadOnly = true;
+
+            if (sqlConnection.State == ConnectionState.Closed)
+                sqlConnection.Open();
+            
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("CustomerViewByID",sqlConnection);
+            sqlDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlDataAdapter.SelectCommand.Parameters.AddWithValue("@CustomerID",tbCustomerID.Text.Trim().ToCharArray());
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.Close();
+            tbCustomerID.Text = tbCustomerID.Text.Trim().ToString();
+            tbCompanyName.Text = dataTable.Rows[0]["CompanyName"].ToString();
+            tbName.Text = dataTable.Rows[0]["ContactName"].ToString();
+            tbContactTitle.Text = dataTable.Rows[0]["ContactTitle"].ToString();
+            tbAddress.Text = dataTable.Rows[0]["Address"].ToString();
+            tbCity.Text = dataTable.Rows[0]["City"].ToString();
+            tbRegion.Text = dataTable.Rows[0]["Region"].ToString();
+            tbPostalCode.Text = dataTable.Rows[0]["PostalCode"].ToString();
+            tbCountry.Text = dataTable.Rows[0]["Country"].ToString();
+            tbPhone.Text = dataTable.Rows[0]["Phone"].ToString();
+            tbFax.Text = dataTable.Rows[0]["Fax"].ToString();
+
+            FillGridView();
+
+        }
+
+        protected void btnNew_Click(object sender, EventArgs e)
+        {
+            Clear();
+            isUpdate = false;
+            btnSave.Enabled = true;
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            isUpdate = true;
+
+            btnNew.Enabled = false;
+            btnDelete.Enabled = false;
+            btnSave.Enabled = true;
+            btnSave.Text = "Update";
+
+            tbCustomerID.ReadOnly = true;
+            tbCompanyName.ReadOnly = false;
+            tbName.ReadOnly = false;
+            tbContactTitle.ReadOnly = false;
+            tbAddress.ReadOnly = false;
+            tbCity.ReadOnly = false;
+            tbRegion.ReadOnly = false;
+            tbPostalCode.ReadOnly = false;
+            tbCountry.ReadOnly = false;
+            tbPhone.ReadOnly = false;
+            tbFax.ReadOnly = false;
+
+            isUpdate = false;
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (isUpdate == true)
+            {
+                UpdateEntry();
+                isUpdate = false;
+                tbCustomerID.ReadOnly = false;
+            }
+            else
+            {
+                CreateEntry();
+            }
+
+            btnNew.Enabled = true;
+            btnDelete.Enabled = true;
+            btnSave.Text = "Save";
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLast_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnPrev_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnFirst_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnPlaceOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
